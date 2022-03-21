9d1c1f5975476d164ac825287f6cfde16be6b264
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-21 14:30:16 -0600

    📅  | Mon Mar 21 14:30:16 MDT 2022

--- ng-products/src/app/products/products.component.ts
+++ ng-products/src/app/products/products.component.ts
@@ -16,12 +16,22 @@ export class ProductsComponent implements OnInit {
   selectedProduct?: Product;
   constructor() {}
 
+  updateProduct(productName: string = '') {
+    console.log(productName);
+    this.selectedProduct = undefined;
+  }
   createProduct(productName: string) {
     console.log(productName);
     const id = Math.floor(Math.random() * 1000);
     this.products.push({ id, name: productName });
   }
 
+  deleteProduct(productId: number) {
+    const prodIndex = this.products.findIndex(
+      (product) => product.id === productId
+    );
+    console.log(this.products.splice(prodIndex, 1));
+  }
   selectProduct(product: Product) {
     this.selectedProduct = product;
     console.log(product);
