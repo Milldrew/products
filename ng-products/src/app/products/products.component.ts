4ec41c14439cf1fff2cb29f6eda60ee012d19345
Andrew Miller <andrew.patrick.miller.2021@gmail.com>
2022-03-21 13:07:11 -0600

    📅  | Mon Mar 21 13:07:11 MDT 2022

--- ng-products/src/app/products/products.component.ts
+++ ng-products/src/app/products/products.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+export type Product = { id: number; name: string };
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
+  products: Product[] = [
+    { id: 1, name: 'product' },
+    { id: 2, name: 'product' },
+    { id: 3, name: 'product' },
+  ];
+  selectedProduct?: Product;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  selectProduct(product: Product) {
+    this.selectedProduct = product;
+    console.log(product);
   }
-
+  ngOnInit(): void {}
 }
